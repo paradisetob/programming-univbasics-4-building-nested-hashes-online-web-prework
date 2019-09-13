136e4d385b4f0dbab96dbaf99ec49f6626b19f81
tracey brimage <onelovenilynn@gmail.com>
2019-09-13 16:57:44 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -6,12 +6,20 @@ def base_hash
 
     }
   }
-  # Remember implicit return! We're returning this new Hash _without_ the
-  # keyword "return." Nice and neat.
-	{}
 end
 
 def monopoly_with_second_tier
+  Colors={
+    Rainbow=>{
+      :Purple=>"Dark",
+      :Yellow=>"light"
+      }
+      "neutral"=>{
+        :white=>"lght neutral",
+        :Black=>"Dark neutral"
+
+      }
+  }
   # When you start writing the implementation for this method, copy the Hash
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
